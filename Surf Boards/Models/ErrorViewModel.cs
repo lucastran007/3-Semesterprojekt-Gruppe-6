6aab4ebb0f829fbb3456f7fe33d09e48c886dbbb
lucastran007 <lucastran007@gmail.com>
2022-08-23 13:34:44 +0200

    Add project files.

---
+++ Surf Boards/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Surf_Boards.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
