eb5f9ca12e01a0fbb3e27fb0e85d8fb846eb8aa1
Kenneth Jacobsen <kshj37934@edu.ucl.dk>
2022-09-12 16:06:30 +0200

    Rental create added

--- Surf Boards/Controllers/RentalController.cs
+++ Surf Boards/Controllers/RentalController.cs
@@ -1,12 +1,60 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Surf_Boards.Areas.Identity.Data;
+using Surf_Boards.Core.Repository;
+using Surf_Boards.Data;
+using Surf_Boards.Models;
+using Surf_Boards.Models.Domain;
 
 namespace Surf_Boards.Controllers
 {
     public class RentalController : Controller
     {
+        private readonly Surf_BoardsContext _context;
+        private readonly UserManager<Surf_BoardsUser> _userManager;
+
+        public RentalController(Surf_BoardsContext context, UserManager<Surf_BoardsUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
         public IActionResult Index()
+        {
+
+            return View();
+        }
+
+        //get Rental/create
+        public IActionResult Create()
         {
             return View();
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Surfboard,StartTime,EndTime,User")] Rental rental)
+        {
+            var user = await _userManager.GetUserAsync(User);
+            var surfboard = Surdboard.Id;
+
+            if (ModelState.IsValid)
+            {
+                //Generate unique ID
+                rental.Id = Guid.NewGuid();
+                //Gets current user
+             
+
+
+                _context.Add(rental);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+
+
+            }
+            return View();
+
+        }
+
     }
 }
--- Surf Boards/Models/Domain/Rental.cs
+++ Surf Boards/Models/Domain/Rental.cs
@@ -1,6 +1,34 @@
-﻿namespace Surf_Boards.Models.Domain
+﻿using Microsoft.AspNetCore.Mvc;
+using Surf_Boards.Areas.Identity.Data;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Surf_Boards.Models.Domain;
+
+namespace Surf_Boards.Models.Domain
 {
     public class Rental
     {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public SurfBoard Surfboard { get; set; }
+
+        [Required]
+        [BindProperty]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:HH:mm dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime StartTime { get; set; }
+        
+        [Required]
+        [BindProperty]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:HH:mm dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime EndTime { get; set; }
+
+        [Required]
+        public Surf_BoardsUser User { get; set; }
+
     }
 }
