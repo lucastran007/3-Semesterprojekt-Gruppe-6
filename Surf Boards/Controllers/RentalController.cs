d6d1bc45b07349ff3c814210e60058757de34291
Kenneth Jacobsen <kshj37934@edu.ucl.dk>
2022-09-16 15:09:58 +0200

    Attempt at delete

--- Surf Boards/Controllers/RentalController.cs
+++ Surf Boards/Controllers/RentalController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
 using Surf_Boards.Areas.Identity.Data;
 using Surf_Boards.Core.Repository;
 using Surf_Boards.Core.ViewModel;
@@ -83,6 +84,44 @@ namespace Surf_Boards.Controllers
             }
             return View();
 
+        }
+
+        // GET: SurfBoards/Delete/5
+        public async Task<IActionResult> Delete(Guid? rentalId)
+        {
+            if (rentalId == null || _context.Rental == null)
+            {
+                return NotFound();
+            }
+
+            var rental = await _context.Rental
+                .FirstOrDefaultAsync(m => m.RentalId == rentalId);
+            if (rental == null)
+            {
+                return NotFound();
+            }
+
+            return View(rental);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid rentalId)
+        {
+            if (_context.Rental == null)
+            {
+                return Problem("Entity set 'Surf_BoardsContext.Rental'  is null.");
+            }
+            var rental = await _context.Rental.FindAsync(rentalId);
+            
+            _context.Rental.Remove(rental);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RentalExists(Guid rentalId)
+        {
+            return _context.Rental.Any(e => e.RentalId == rentalId);
         }
 
     }
