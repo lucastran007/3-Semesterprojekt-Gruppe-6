41e21a1529c08c44a634db691fac5b67f7753bd1
lucastran007 <lucastran007@gmail.com>
2022-09-30 13:22:37 +0200

    RentalController CRUD funktion tilføjet API

--- Surf Boards API/Controllers/RentalController.cs
+++ Surf Boards API/Controllers/RentalController.cs
@@ -16,6 +16,9 @@ namespace Surf_Boards_API.Controllers
             _context = context;
         }
         // Get all Rentals 
+
+
+
         [HttpGet]
         public async Task<IActionResult> GetRentals()
         { var rentals = await _context.Rental.ToArrayAsync();
@@ -24,12 +27,67 @@ namespace Surf_Boards_API.Controllers
 
         // Get rental by GUID Id
         [HttpGet("{id}")]
-        public async Task<IActionResult> GetRental(Guid id) 
+        public async Task<IActionResult> GetRental(Guid id)
         {
             var rental = await _context.Rental.FindAsync(id);
             if (rental == null)
-                return NotFound();  
+                return NotFound();
             return Ok(rental);
         }
+
+
+        [HttpPost]
+        public async Task<IActionResult> AddRental(Rental rental)
+        {
+            _context.Rental.Add(rental);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetRental",
+                new { id = rental.RentalId },
+                rental);
+        }
+
+        [HttpPut("{id}")]
+        public async Task <IActionResult> UpdateRental(Guid id, [FromBody] Rental rental)
+        {
+             if (id != rental.RentalId)
+            {
+                return BadRequest();
+            }
+
+             _context.Entry(rental).State = EntityState.Modified;
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+
+                if (!_context.Rental.Any(r => r.RentalId == id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return Ok(rental);
+
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteRental([FromBody] Guid id)
+        {
+            var rentals = await _context.Rental.FindAsync(id);
+            if (rentals != null)
+            {
+                _context.Remove(rentals);
+                await _context.SaveChangesAsync();
+                return Ok(rentals);
+            }
+            return Ok(NotFound());
+        }
+
     }
 }
