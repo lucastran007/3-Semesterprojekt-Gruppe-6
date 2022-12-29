1da335a9b454fb0ff9fe657cdcc269626063d9d0
gEE <mail@mickeygee.com>
2022-12-29 11:13:25 +0100

    Indent correction #OCD

--- Blazor/Server/Data/ApplicationDbContext.cs
+++ Blazor/Server/Data/ApplicationDbContext.cs
@@ -9,9 +9,7 @@ namespace Blazor.Server.Data
 {
     public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
     {
-        public ApplicationDbContext(
-            DbContextOptions options,
-            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        public ApplicationDbContext(DbContextOptions options, IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
         {
         }
 
