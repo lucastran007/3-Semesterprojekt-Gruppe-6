d96fd269081ca247965fb9c78a9bd393397f89f0
Kenneth Jacobsen <kshj37934@edu.ucl.dk>
2022-12-29 14:39:53 +0100

    Initial pupup - not working

--- Blazor/Client/Program.cs
+++ Blazor/Client/Program.cs
@@ -2,6 +2,9 @@ using Blazor.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Blazored.Modal;
+
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -12,6 +15,8 @@ builder.Services.AddHttpClient("Blazor.ServerAPI", client => client.BaseAddress 
 
 // Supply HttpClient instances that include access tokens when making requests to the server project
 builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("Blazor.ServerAPI"));
+builder.Services.AddBlazoredModal();
+
 
 builder.Services.AddApiAuthorization();
 
